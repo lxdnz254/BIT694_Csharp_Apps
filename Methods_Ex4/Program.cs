eabbd4a2cae2567ab63d7073b03d9367147e5abf
lxdnz254 <lxdnz34@gmail.com>
2017-06-21 12:58:53 +1200

    Method Ex_4

---
+++ Methods_Ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Ex4
+{
+    class Program
+    {
+        static void method1(int n)
+        {
+            Console.WriteLine("Hi, I am in method and the value of n is: "+n);
+            method1(n-1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I am in the main function\n");
+            method1(5);
+            Console.WriteLine("Press any key to continue .....");
+            Console.ReadKey();
+        } //end of main method
+    } // end of class
+}
---
+++ Methods_Ex4/output.md
@@ -0,0 +1,3 @@
+# Method Ex_4
+
+A continous loop which starts at **n** in the main method, and outputs *Hi, I am in method an the value of n is: (n-1)* continuously until application is stopped (Ctrl-C).

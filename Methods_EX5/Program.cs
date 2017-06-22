92c40679ee762bcbc3f2e0c0b19fd011828a0267
lxdnz254 <lxdnz34@gmail.com>
2017-06-22 19:37:39 +1200

    Recursive Methods investigated

---
+++ Methods_EX5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_EX5
+{
+    class Program
+    {
+        static void method1(int n)
+        {
+            Console.WriteLine("the value of n is: " + n);
+            if (n > 0)
+            {
+                method1(n-1);
+            }
+            return;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I am in the main function\n");
+            method1(5);
+            Console.WriteLine("Press any key ....");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Methods_EX6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_EX6
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if (n ==1) return (1);
+            return (n*factorial(n-1));
+        }
+
+        static int fibonacci(int n)
+        {
+            if (n <=2)  return (1);
+            return (fibonacci(n-1) + fibonacci(n-2));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the main method");
+            Console.WriteLine("The factorial of 5 is: " + factorial(5));
+            Console.WriteLine("The fibonacci sequence number at position 2 is: " + fibonacci(2));
+            Console.WriteLine("Press any key to continue .....");
+            Console.ReadKey();
+        }
+    }
+}
